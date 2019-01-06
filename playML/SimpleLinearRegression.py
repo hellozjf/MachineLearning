d961a2fabd6aa68edbf34c7a86a14e4ddc5974dd
hellozjf <908686171@qq.com>
2019-01-06 23:30:51 +0800

    update 学习了梯度下降

--- playML/SimpleLinearRegression.py
+++ playML/SimpleLinearRegression.py
@@ -1,6 +1,59 @@
 import numpy as np
+from .metrics import r2_score
 
 
+# 使用向量方式计算的线性回归，速度比自己用for循环更快
+class SimpleLinearRegression:
+
+    def __init__(self):
+        """初始化Simple Linear Regression 模型"""
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        """根据训练数据集x_train, y_train训练Simple Linear Regression"""
+        assert x_train.ndim == 1, \
+            "Simple Linear Regressor can only solve single feature training data."
+        assert len(x_train) == len(y_train), \
+            "the size of x_train must be equal to the size of y_train"
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        # 分子 num
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        # 分母 d
+        d = (x_train - x_mean).dot(x_train - x_mean)
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        """给定待遇测数据集x_predict，返回表示x_predict的结果向量"""
+        assert x_predict.ndim == 1, \
+            "Simple Linear Regressor can only solve single feature training data."
+        assert self.a_ is not None and self.b_ is not None, \
+            "must fit before predict!"
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        """给定单个待遇测数据x_single，返回x的预测结果值"""
+        return self.a_ * x_single + self.b_
+
+    def score(self, x_test, y_test):
+        """根据测试数据集x_test和y_test确定当前模型的准确度"""
+
+        y_predict = self.predict(x_test)
+        return r2_score(y_test, y_predict)
+
+    def __repr__(self):
+        return "SimpleLinearRegression2()"
+
+
+# 使用for循环写的线性回归
 class SimpleLinearRegression1:
 
     def __init__(self):
@@ -46,48 +99,4 @@ class SimpleLinearRegression1:
 
     def __repr__(self):
         return "SimpleLinearRegression1()"
-
-
-class SimpleLinearRegression2:
-
-    def __init__(self):
-        """初始化Simple Linear Regression 模型"""
-        self.a_ = None
-        self.b_ = None
-
-    def fit(self, x_train, y_train):
-        """根据训练数据集x_train, y_train训练Simple Linear Regression"""
-        assert x_train.ndim == 1, \
-            "Simple Linear Regressor can only solve single feature training data."
-        assert len(x_train) == len(y_train), \
-            "the size of x_train must be equal to the size of y_train"
-
-        x_mean = np.mean(x_train)
-        y_mean = np.mean(y_train)
-
-        # 分子 num
-        num = (x_train - x_mean).dot(y_train - y_mean)
-        # 分母 d
-        d = (x_train - x_mean).dot(x_train - x_mean)
-
-        self.a_ = num / d
-        self.b_ = y_mean - self.a_ * x_mean
-
-        return self
-
-    def predict(self, x_predict):
-        """给定待遇测数据集x_predict，返回表示x_predict的结果向量"""
-        assert x_predict.ndim == 1, \
-            "Simple Linear Regressor can only solve single feature training data."
-        assert self.a_ is not None and self.b_ is not None, \
-            "must fit before predict!"
-
-        return np.array([self._predict(x) for x in x_predict])
-
-    def _predict(self, x_single):
-        """给定单个待遇测数据x_single，返回x的预测结果值"""
-        return self.a_ * x_single + self.b_
-
-    def __repr__(self):
-        return "SimpleLinearRegression2()"
 
